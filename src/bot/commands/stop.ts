0f25af2fd8afb9e68983074a68e508dde529214c
Ishan Shishodiya <sly.of.zero@gmail.com>
2024-01-17 01:47:21 +0530

    fixes

--- src/bot/commands/stop.ts
+++ src/bot/commands/stop.ts
@@ -1,12 +1,10 @@
-import { addDocument, getDocument, removeDocumentById } from "@/firebase";
+import { getDocument, removeDocumentById } from "@/firebase";
 import { BotCommandContextType, StoredGroup } from "@/types";
 import { cleanUpBotMessage } from "@/utils/bot";
 import { BOT_USERNAME } from "@/utils/env";
-import { Address } from "@ton/ton";
 import { onlyAdmin } from "../utils";
 
 export async function stopBot(ctx: BotCommandContextType) {
-  const { match: jetton } = ctx;
   const { id: chatId, type } = ctx.chat;
   let text = "";
 
@@ -20,29 +18,23 @@ export async function stopBot(ctx: BotCommandContextType) {
 
     text = `Messages from ${BOT_USERNAME} stopped for this ${type}. Use /start to start.`;
 
-    try {
-      const newAddress = Address.parse(jetton).toRawString();
-      const projectData =
-        ((
-          await getDocument({
-            collectionName: "project_groups",
-            queries: [["chatId", "==", String(chatId)]],
-          })
-        ).at(0) as StoredGroup) || undefined;
-
-      if (projectData) {
-        ctx.reply(cleanUpBotMessage(text), { parse_mode: "MarkdownV2" });
-        removeDocumentById({
+    const projectData =
+      ((
+        await getDocument({
           collectionName: "project_groups",
-          id: projectData.id || "",
-        });
-      } else {
-        ctx.reply(cleanUpBotMessage(text), { parse_mode: "MarkdownV2" });
-        const data: StoredGroup = { chatId: String(chatId), jetton: newAddress };
-        addDocument({ data, collectionName: "project_groups" });
-      }
-    } catch (error) {
-      ctx.reply("The jetton address you passed was incorrect.");
+          queries: [["chatId", "==", String(chatId)]],
+        })
+      ).at(0) as StoredGroup) || undefined;
+
+    if (projectData) {
+      ctx.reply(cleanUpBotMessage(text), { parse_mode: "MarkdownV2" });
+      removeDocumentById({
+        collectionName: "project_groups",
+        id: projectData.id || "",
+      });
+    } else {
+      text = `${BOT_USERNAME} isn't running here`;
+      ctx.reply(cleanUpBotMessage(text), { parse_mode: "MarkdownV2" });
     }
   }
 }
