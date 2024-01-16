946adf0c2e18de9c90306f3e23f2da5356ec06ef
Ishan Shishodiya <sly.of.zero@gmail.com>
2024-01-17 01:34:44 +0530

    fixes

--- src/bot/commands/index.ts
+++ src/bot/commands/index.ts
@@ -4,16 +4,19 @@ import { log } from "@/utils/handlers";
 import { settings } from "./settings";
 import { setEmojiCommand } from "./setEmoji";
 import { setGifCommand } from "./setGif";
+import { stopBot } from "./stop";
 
 export function initiateBotCommands() {
   teleBot.api.setMyCommands([
     { command: "start", description: "Start the bot" },
+    { command: "stop", description: "Stop the bot" },
     { command: "settings", description: "To customize the bot" },
     { command: "setemoji", description: "To set an emoji" },
     { command: "setgif", description: "To set a GIF" },
   ]);
 
   teleBot.command("start", (ctx) => startBot(ctx));
+  teleBot.command("stop", (ctx) => stopBot(ctx));
   teleBot.command("settings", (ctx) => settings(ctx));
   teleBot.command("setemoji", (ctx) => setEmojiCommand(ctx));
   teleBot.hears(/\/setgif/, (ctx) => setGifCommand(ctx));
