afbfd196dcf90507acd54b90b1e6dd0350b24694
Ishan Shishodiya <sly.of.zero@gmail.com>
2024-01-16 11:35:42 +0530

    Added bot commands

--- src/bot/commands/index.ts
+++ src/bot/commands/index.ts
@@ -3,6 +3,7 @@ import { startBot } from "./start";
 import { log } from "@/utils/handlers";
 
 export function initiateBotCommands() {
+  teleBot.api.setMyCommands([{ command: "start", description: "Start the bot" }]);
   teleBot.command("start", (ctx) => startBot(ctx));
 
   log("Bot commands up");
