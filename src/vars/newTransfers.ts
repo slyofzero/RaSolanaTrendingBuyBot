ca84c87d07d0b563d0899cf42b5fb65ab29d7e13
Ishan Shishodiya <sly.of.zero@gmail.com>
2024-05-04 13:56:18 +0530

    fixes

--- src/vars/newTransfers.ts
+++ src/vars/newTransfers.ts
@@ -1,27 +1,27 @@
-import { scanNewTransfer } from "@/bot";
-import { NewTransfer } from "@/types/var";
-import { log } from "@/utils/handlers";
+// import { scanNewTransfer } from "@/bot";
+// import { NewTransfer } from "@/types/var";
+// import { log } from "@/utils/handlers";
 
-export let newTransfers: NewTransfer[] = [];
+// export let newTransfers: NewTransfer[] = [];
 
-export function addNewTransfer(newTransfer: NewTransfer) {
-  newTransfers.push(newTransfer);
-}
+// export function addNewTransfer(newTransfer: NewTransfer) {
+//   newTransfers.push(newTransfer);
+// }
 
-export async function checkNewTransfer() {
-  log(`Checking for new transfers, got - ${newTransfers.length}`);
+// export async function checkNewTransfer() {
+//   log(`Checking for new transfers, got - ${newTransfers.length}`);
 
-  for (const [index, transfer] of newTransfers.entries()) {
-    const parsed = await scanNewTransfer(transfer);
-    const transferToUpdate = newTransfers.at(index);
+//   for (const [index, transfer] of newTransfers.entries()) {
+//     const parsed = await scanNewTransfer(transfer);
+//     const transferToUpdate = newTransfers.at(index);
 
-    if (transferToUpdate) {
-      transferToUpdate.parsed = Boolean(parsed);
-    }
-  }
+//     if (transferToUpdate) {
+//       transferToUpdate.parsed = Boolean(parsed);
+//     }
+//   }
 
-  // Removing all parsed transfers
-  newTransfers = [];
+//   // Removing all parsed transfers
+//   newTransfers = [];
 
-  log("New transfers cleaned up");
-}
+//   log("New transfers cleaned up");
+// }
