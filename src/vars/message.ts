9fdfb7414f74572aa3abadc12cef0b8789ddcfa8
slyofzero <sly.of.zero@gmail.com>
2024-08-13 12:37:23 +0530

    Added error handling for trending API fetch

--- src/vars/message.ts
+++ src/vars/message.ts
@@ -1,13 +1,18 @@
 import { apiFetcher } from "@/utils/api";
 import { TRENDING_AUTH_KEY, TRENDING_TOKENS_API } from "@/utils/env";
+import { errorHandler } from "@/utils/handlers";
 
 export let trendingMessageId: number = 0;
 export async function syncTrendingMessageId() {
-  const { messageId } = (
-    await apiFetcher(`${TRENDING_TOKENS_API}/getLastMessage`, {
-      Authorization: TRENDING_AUTH_KEY || "",
-    })
-  ).data as { messageId: number };
+  try {
+    const { messageId } = (
+      await apiFetcher(`${TRENDING_TOKENS_API}/getLastMessage`, {
+        Authorization: TRENDING_AUTH_KEY || "",
+      })
+    ).data as { messageId: number };
 
-  trendingMessageId = Number(messageId);
+    trendingMessageId = Number(messageId);
+  } catch (error) {
+    errorHandler(error);
+  }
 }
